22dd3f1b55ab8fc1c86f05a35651ad0e88120db7
aleyna çelik <celikaleyna71@gmail.com>
2023-07-10 16:22:59 +0300

    Gün sonu

--- Casgem_Portfolio/Controllers/ServiceController.cs
+++ Casgem_Portfolio/Controllers/ServiceController.cs
@@ -15,9 +15,45 @@ namespace Casgem_Portfolio.Controllers
             var values = db.TblService.ToList();
             return View(values);
         }
+        [HttpGet]
         public ActionResult AddService()
         {
             return View();
+        }//sayfa çalışınca açılacak
+
+        [HttpPost]
+        public ActionResult AddService(TblService p)
+        {
+            db.TblService.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }  //sayfada butona tıklayınca çalışacak
+        public ActionResult DeleteService(int id)
+        {
+            var value = db.TblService.Find(id);
+            db.TblService.Remove(value);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+        [HttpGet]
+        public ActionResult UpdateService(int id)
+        {
+            var value = db.TblService.Find(id);
+            return View(value);
+        }
+        [HttpPost]
+        public ActionResult UpdateService(TblService p)
+        {
+            var value = db.TblService.Find(p.ServiceID);
+            value.ServiceTitle = p.ServiceTitle;
+            value.ServiceIcon = p.ServiceIcon;
+            value.ServiceNumber = p.ServiceNumber;
+            value.ServiceContent = p.ServiceContent;
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
+
+
 }
