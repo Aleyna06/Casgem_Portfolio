6e44d8d078ac1a7a8ba539bd2c69b7da7ca79ca5
aleyna çelik <celikaleyna71@gmail.com>
2023-07-10 22:07:39 +0300

    Referanslar kısmı tamamlandı

---
+++ Casgem_Portfolio/Controllers/ReferanceController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Casgem_Portfolio.Models.Entities;
+
+namespace Casgem_Portfolio.Controllers
+{
+    public class ReferanceController : Controller
+    {
+        CasgemPortfolioEntities db = new CasgemPortfolioEntities();
+        public ActionResult Index()
+        {
+            var values = db.TblReferance.ToList();
+            return View(values);
+
+
+        }
+        [HttpGet]
+        public ActionResult AddReferance()
+        {
+            return View();
+        }//sayfa çalışınca açılacak
+
+        [HttpPost]
+        public ActionResult AddReferance(TblReferance p)
+        {
+            db.TblReferance.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }  //sayfada butona tıklayınca çalışacak
+        public ActionResult DeleteReferance(int id)
+        {
+
+            var value = db.TblReferance.Find(id); //Birincil anahtarın olduğu sütunu buluyo 
+            db.TblReferance.Remove(value);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult UpdateReferance(int id)
+        {
+            var value = db.TblReferance.Find(id);
+            return View(value);
+        }
+        [HttpPost]
+        public ActionResult UpdateReferance(TblReferance p)
+        {
+            var value = db.TblReferance.Find(p.ReferansID);
+            value.ReferanceSirket = p.ReferanceSirket;
+            value.ReferancePozisyon = p.ReferancePozisyon;
+            value.ReferanceEposta = p.ReferanceEposta;
+           
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
--- Casgem_Portfolio/Models/Entities/Model1.Context.cs
+++ Casgem_Portfolio/Models/Entities/Model1.Context.cs
@@ -28,5 +28,6 @@ namespace Casgem_Portfolio.Models.Entities
         public virtual DbSet<TblFeature> TblFeature { get; set; }
         public virtual DbSet<TblService> TblService { get; set; }
         public virtual DbSet<TblMessage> TblMessage { get; set; }
+        public virtual DbSet<TblReferance> TblReferance { get; set; }
     }
 }
