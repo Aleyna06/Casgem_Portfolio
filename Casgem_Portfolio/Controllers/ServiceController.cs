d9d73f63371292c0ea71388ddf2a7d3385d3edeb
aleyna çelik <celikaleyna71@gmail.com>
2023-07-10 14:34:51 +0300

    Add project files.

---
+++ Casgem_Portfolio/Controllers/ServiceController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Casgem_Portfolio.Models.Entities;
+namespace Casgem_Portfolio.Controllers
+{
+    public class ServiceController : Controller
+    {
+        CasgemPortfolioEntities db = new CasgemPortfolioEntities(); //db isimli nesne üretttim 
+        // GET: Service
+        public ActionResult Index()
+        {
+            var values = db.TblService.ToList();
+            return View(values);
+        }
+        public ActionResult AddService()
+        {
+            return View();
+        }
+    }
+}
